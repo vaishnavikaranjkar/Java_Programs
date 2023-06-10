ef608e5f697021607ee3f58f3bce5f5470dd08ff
Vaishnavi Karanjkar <karanjkarv4@gmail.com>
2023-06-11 00:43:17 +0530

    added arrayList

--- arrayList.java
+++ arrayList.java
@@ -8,6 +8,14 @@ array list- it is a part of java collections framework. it is a class found in u
 two types-
 non-generic (with multiple data types)
 generic (with specific data type)
+
+methods - 
+1. add() - add(index,element):inserts an element in given index and shifts existing element(if any) in next index.
+2. size()
+3. isEmpty() - gives boolean value
+4. remove() - remove("c"), remove(index_of_element)
+5. get() - access element in arraylist, get(index)
+6. set() - update a value, set(index, value)
 */
 
 import java.util.ArrayList;
@@ -20,6 +28,7 @@ public class arrayList {
         ArrayList a = new ArrayList();
         a.add(45);
         a.add("meow");
+        a.add(1,2);
 
         for (var object : a) {
             System.out.println(object);
@@ -31,6 +40,7 @@ public class arrayList {
         ArrayList<Integer> arr1=new ArrayList<Integer>();
         arr1.add(12);
         arr1.add(45);
+        arr1.add(1,55);
         for (Integer integer : arr1) {
             System.out.println(integer);
         }
