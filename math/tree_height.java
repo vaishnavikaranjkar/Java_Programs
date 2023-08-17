e0e19f0be2c64a8f5ac735d6971198fa9eabdfd4
Vaishnavi Karanjkar <karanjkarv4@gmail.com>
2023-08-17 13:34:49 +0530

    added tree height and alphabet_height codes

---
+++ math/tree_height.java
@@ -0,0 +1,64 @@
+/*
+ * The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer, its height increases by 1 meter.
+
+A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring. How tall will the tree be after n  growth cycles?
+ */
+
+import java.io.*;
+import java.util.stream.*;
+
+class Result {
+
+    /*
+     * Complete the 'utopianTree' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int utopianTree(int n) {
+    // Write your code here
+        int height=1;
+        if(n==0)
+        {
+            return height;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+                height=height+1;
+            }
+            else{
+                height=height*2;
+            }
+        }
+        return height;
+    }
+
+}
+
+public class tree_height {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.utopianTree(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ strings/alphabet_height.java
@@ -0,0 +1,56 @@
+/*
+ * There is a list of 26 character heightsarray aligned by index to their letters. For example, 'a' is at index 0 and 'z' is at index 25. There will also be a string. Using the letter heights given, determine the area of the rectangle highlight in mm^2 assuming all letters are 1mm wide.
+ */
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<Integer> h, String word) {
+    // Write your code here
+        int len,temp,max_height=0;
+        for(int i=0;i<word.length();i++)
+        {
+            temp=word.charAt(i)-97;
+            len=h.get(temp);
+            if(len>max_height)
+            {
+                max_height=len;
+            }
+        }
+        return max_height*word.length();
+    }
+
+}
+
+public class alphabet_height {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String word = bufferedReader.readLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
