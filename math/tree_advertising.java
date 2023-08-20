a86c6caae8a8dcc4bd9c2e7a607b9eed52d5ac6a
Vaishnavi Karanjkar <karanjkarv4@gmail.com>
2023-08-20 18:50:14 +0530

    added tree-advertising code

---
+++ math/tree_advertising.java
@@ -0,0 +1,48 @@
+/*
+ * HackerLand Enterprise is adopting a new viral advertising strategy. When they launch a new product, they advertise it to exactly 5 people on social media.
+
+On the first day, half of those 5 people (i.e., floor(5/2)) like the advertisement and each shares it with 3 of their friends. At the beginning of the second day, floor(5/2)*3=6 people receive the advertisement.
+
+Each day, floor(recipients/2) of the recipients like the advertisement and will share it with 3 friends on the following day. Assuming nobody receives the advertisement twice, determine how many people have liked the ad by the end of a given day, beginning with launch day as day 1.
+ */
+import java.io.*;
+class Result {
+
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int viralAdvertising(int n) {
+    // Write your code here
+        int temp,liked=0;
+        int a=5;
+        for(int i=0;i<n;i++)
+        {
+            temp=Math.floorDiv(a, 2);
+            a=temp*3;
+            liked=liked+temp;
+        }
+        return liked;
+    }
+
+}
+
+public class tree_advertising {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
