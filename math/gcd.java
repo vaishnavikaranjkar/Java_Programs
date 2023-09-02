95b4a7abae125d0b287b7ff8bb0eb8c1004da89e
Vaishnavi Karanjkar <karanjkarv4@gmail.com>
2023-09-02 17:35:53 +0530

    added gcd code

---
+++ math/gcd.java
@@ -0,0 +1,25 @@
+
+public class gcd {
+    public static void main(String[] args) {
+        int a=98, b=56;
+        int res;
+        int min=Math.min(a, b);
+        if(a%min==0 && b%min==0)
+        {
+            res=min;
+            System.out.println(res);
+        }
+        else
+        {
+            for(int i=min/2;i>=2;i--)
+            {
+                if(a%i==0 && b%i==0)
+                {
+                    res=i;
+                    System.out.println(i+" "+res);
+                    break;
+                }
+            }
+        }
+    }
+}
